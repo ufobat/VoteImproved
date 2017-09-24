cad312c981c66026389eac61eedcdeb211d0a737
Martin Barth <martin@senfdax.de>
2017-09-24 11:13:39 +0200

    removed some console.log() statements

--- public/js/mylib.js
+++ public/js/mylib.js
@@ -4,14 +4,12 @@ function change_image_rating(value, imageid) {
     data.append('image', imageid );
     data.append('rating', value);
     xhr.open('POST','/vote/vote_image/' + imageid, true);
-    console.log('rating: ' + value);
     // xhr.setRequestHeader('Content-Type', data.contentType);
     xhr.onreadystatechange = function() {//Call a function when the state changes.
         if(xhr.readyState == 4) {
-            console.log(xhr.status);
-            console.log(xhr.responseText);
+            // console.log(xhr.status);
+            // console.log(xhr.responseText);
         }
     }
     xhr.send(data);
-    console.log(value);
 }
